f2dc8cd50c35b4eda93fa43ad9db56e12de8de7e
RosaMoran <rosa.moran.delgado@gmail.com>
2022-05-30 22:51:29 +0200

    end of possibility

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -4,7 +4,12 @@ import React from 'react'
 
 const CTA = () => {
   return (
-    <div>CTA</div>
+    <div className="gpt3__cta">
+      <div className="gpt3__cta-content">
+        <p>Request Early Access to Get Started</p>
+        <h3>Register Today & start exploring the endless possibilities.</h3>
+      </div>
+    </div>
   )
 }
 
