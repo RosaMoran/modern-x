e60df025b4c35962f66421feff1a823a0f18048d
RosaMoran <rosa.moran.delgado@gmail.com>
2022-06-02 16:40:01 +0200

    nothing

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,29 +1,23 @@
 import React, { useState } from 'react';
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
-import logo from '../../assets/logo.svg';
+import logo from '../../logo.svg';
 import './navbar.css';
-const Navbar = () => {
-  const Menu = () =>(
-    <>
-    <p><a href="#home">Home</a></p>
-    <p><a href="#wgpt3">What is GPT3?</a></p>
-    <p><a href="#possibility">Open AI</a></p>
-    <p><a href="#features">Case Studies</a></p>
-    <p><a href="#blog">Library</a></p>
-    </>
-    
-    )
 
+const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
 
   return (
     <div className="gpt3__navbar">
       <div className="gpt3__navbar-links">
         <div className="gpt3__navbar-links_logo">
-          <img src={logo} alt="logo"/>
+          <img src={logo} />
         </div>
         <div className="gpt3__navbar-links_container">
-          <Menu/>
+          <p><a href="#home">Home</a></p>
+          <p><a href="#wgpt3">What is GPT3?</a></p>
+          <p><a href="#possibility">Open AI</a></p>
+          <p><a href="#features">Case Studies</a></p>
+          <p><a href="#blog">Library</a></p>
         </div>
       </div>
       <div className="gpt3__navbar-sign">
@@ -37,11 +31,11 @@ const Navbar = () => {
         {toggleMenu && (
         <div className="gpt3__navbar-menu_container scale-up-center">
           <div className="gpt3__navbar-menu_container-links">
-            <Menu/>
-            <div className="gpt3__navbar-menu_container-links-sign">
-        <p>Sign in</p>
-        <button type="button">Sign up</button>
-      </div>
+            <p><a href="#home">Home</a></p>
+            <p><a href="#wgpt3">What is GPT3?</a></p>
+            <p><a href="#possibility">Open AI</a></p>
+            <p><a href="#features">Case Studies</a></p>
+            <p><a href="#blog">Library</a></p>
           </div>
           <div className="gpt3__navbar-menu_container-links-sign">
             <p>Sign in</p>
--- src/index.js
+++ src/index.js
@@ -1,15 +1,7 @@
-import React from "react"; 
-import ReactDOM from "react-dom/client"
-import reportWebVitals from "./reportWebVitals";
-import App from "./App"
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+import App from './App';
+import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <React.StrictMode>
-        <App/>
-    </React.StrictMode>
-);
-
-reportWebVitals();
+ReactDOM.render(<App />, document.getElementById('root'));
